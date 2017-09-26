4473c3c7088adba06c8f3058bfc53b81028d6323
robgmerrill <robgmerrill@gmail.com>
2017-09-25 19:49:00 -0700

    created expense add/edit form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import ExpenseForm from './ExpenseForm'
 
 const AddExpensePage = () => (
   <div>
-    Add expense component
+    <h1>Add Expense</h1>
+    <ExpenseForm/>
   </div>
 );
 
