f2b6002e3069131ba9e5b80ff8bf1694c3f32a36
robgmerrill <robgmerrill@gmail.com>
2017-09-25 18:47:52 -0700

    controlled inputs for filter

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import ExpenseList from './ExpenseList';
-import ExpenseListItem from './ExpenseListItem';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseListFilters/>
     <ExpenseList/>
-    <ExpenseListItem/>
   </div>
 );
 
