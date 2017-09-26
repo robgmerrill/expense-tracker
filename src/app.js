b7eb1bbbc9db401efee510271daadf48aab6f4d2
robgmerrill <robgmerrill@gmail.com>
2017-09-25 17:51:05 -0700

    connected store and component with react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -13,9 +14,18 @@ store.dispatch(addExpense({ description: 'car'}));
 store.dispatch(addExpense({ description: 'car wash'}));
 store.dispatch(setTextFilter('ca'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('a'));
+}, 3000)
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+  <Provider store={store}>
+    <AppRouter/>
+  </Provider>
+);
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
